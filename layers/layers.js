e5ff1f626e3645901ff144603504540f6d2107c1
Poivan98 <135073939+Poivan98@users.noreply.github.com>
2026-02-18 12:22:22 +0100

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -48,7 +48,7 @@ var lyr_VescomtatdeBas_3 = new ol.layer.Vector({
                 source:jsonSource_VescomtatdeBas_3, 
                 style: style_VescomtatdeBas_3,
                 popuplayertitle: 'Vescomtat de Bas',
-                interactive: false,
+                interactive: true,
                 title: '<img src="styles/legend/VescomtatdeBas_3.png" /> Vescomtat de Bas'
             });
 var format_SenyoriudUrtx_4 = new ol.format.GeoJSON();
@@ -63,7 +63,7 @@ var lyr_SenyoriudUrtx_4 = new ol.layer.Vector({
                 source:jsonSource_SenyoriudUrtx_4, 
                 style: style_SenyoriudUrtx_4,
                 popuplayertitle: 'Senyoriu d\'Urtx',
-                interactive: false,
+                interactive: true,
                 title: '<img src="styles/legend/SenyoriudUrtx_4.png" /> Senyoriu d\'Urtx'
             });
 var format_VescomtatdeCardona_5 = new ol.format.GeoJSON();
@@ -78,7 +78,7 @@ var lyr_VescomtatdeCardona_5 = new ol.layer.Vector({
                 source:jsonSource_VescomtatdeCardona_5, 
                 style: style_VescomtatdeCardona_5,
                 popuplayertitle: 'Vescomtat de Cardona',
-                interactive: false,
+                interactive: true,
                 title: '<img src="styles/legend/VescomtatdeCardona_5.png" /> Vescomtat de Cardona'
             });
 var format_VescomtatdeCabrera_6 = new ol.format.GeoJSON();
@@ -93,7 +93,7 @@ var lyr_VescomtatdeCabrera_6 = new ol.layer.Vector({
                 source:jsonSource_VescomtatdeCabrera_6, 
                 style: style_VescomtatdeCabrera_6,
                 popuplayertitle: 'Vescomtat de Cabrera',
-                interactive: false,
+                interactive: true,
                 title: '<img src="styles/legend/VescomtatdeCabrera_6.png" /> Vescomtat de Cabrera'
             });
 var format_VescomtatdeRocabert_7 = new ol.format.GeoJSON();
@@ -108,7 +108,7 @@ var lyr_VescomtatdeRocabert_7 = new ol.layer.Vector({
                 source:jsonSource_VescomtatdeRocabert_7, 
                 style: style_VescomtatdeRocabert_7,
                 popuplayertitle: 'Vescomtat de Rocabertí',
-                interactive: false,
+                interactive: true,
                 title: '<img src="styles/legend/VescomtatdeRocabert_7.png" /> Vescomtat de Rocabertí'
             });
 var format_BaroniadEntena_8 = new ol.format.GeoJSON();
@@ -123,7 +123,7 @@ var lyr_BaroniadEntena_8 = new ol.layer.Vector({
                 source:jsonSource_BaroniadEntena_8, 
                 style: style_BaroniadEntena_8,
                 popuplayertitle: 'Baronia d\'Entença',
-                interactive: false,
+                interactive: true,
                 title: '<img src="styles/legend/BaroniadEntena_8.png" /> Baronia d\'Entença'
             });
 var format_ComtatdUrgell_9 = new ol.format.GeoJSON();
@@ -141,38 +141,38 @@ var lyr_ComtatdUrgell_9 = new ol.layer.Vector({
                 interactive: true,
                 title: '<img src="styles/legend/ComtatdUrgell_9.png" /> Comtat d\'Urgell'
             });
-var group_Corts1250 = new ol.layer.Group({
+var group_Corts1300 = new ol.layer.Group({
                                 layers: [lyr_VescomtatdeBas_3,lyr_SenyoriudUrtx_4,lyr_VescomtatdeCardona_5,lyr_VescomtatdeCabrera_6,lyr_VescomtatdeRocabert_7,lyr_BaroniadEntena_8,lyr_ComtatdUrgell_9,],
                                 fold: 'open',
-                                title: 'Corts 1250'});
+                                title: 'Corts 1300'});
 var group_Mapesbase = new ol.layer.Group({
                                 layers: [lyr_ESRISatellite_0,lyr_ESRIOcean_1,lyr_ESRITerrain_2,],
                                 fold: 'open',
                                 title: 'Mapes base'});
 
 lyr_ESRISatellite_0.setVisible(true);lyr_ESRIOcean_1.setVisible(true);lyr_ESRITerrain_2.setVisible(true);lyr_VescomtatdeBas_3.setVisible(true);lyr_SenyoriudUrtx_4.setVisible(true);lyr_VescomtatdeCardona_5.setVisible(true);lyr_VescomtatdeCabrera_6.setVisible(true);lyr_VescomtatdeRocabert_7.setVisible(true);lyr_BaroniadEntena_8.setVisible(true);lyr_ComtatdUrgell_9.setVisible(true);
-var layersList = [group_Mapesbase,group_Corts1250];
-lyr_VescomtatdeBas_3.set('fieldAliases', {'CODIMUNI': 'CODIMUNI', 'NOMMUNI': 'NOMMUNI', 'CAPMUNI': 'CAPMUNI', 'AREAM5000': 'AREAM5000', 'CODICOMAR': 'CODICOMAR', 'NOMCOMAR': 'NOMCOMAR', 'CAPCOMAR': 'CAPCOMAR', 'CODIVEGUE': 'CODIVEGUE', 'NOMVEGUE': 'NOMVEGUE', 'CAPVEGUE': 'CAPVEGUE', 'CODIPROV': 'CODIPROV', 'NOMPROV': 'NOMPROV', 'CAPPROV': 'CAPPROV', });
-lyr_SenyoriudUrtx_4.set('fieldAliases', {'CODIMUNI': 'CODIMUNI', 'NOMMUNI': 'NOMMUNI', 'CAPMUNI': 'CAPMUNI', 'AREAM5000': 'AREAM5000', 'CODICOMAR': 'CODICOMAR', 'NOMCOMAR': 'NOMCOMAR', 'CAPCOMAR': 'CAPCOMAR', 'CODIVEGUE': 'CODIVEGUE', 'NOMVEGUE': 'NOMVEGUE', 'CAPVEGUE': 'CAPVEGUE', 'CODIPROV': 'CODIPROV', 'NOMPROV': 'NOMPROV', 'CAPPROV': 'CAPPROV', });
-lyr_VescomtatdeCardona_5.set('fieldAliases', {'CODIMUNI': 'CODIMUNI', 'NOMMUNI': 'NOMMUNI', 'CAPMUNI': 'CAPMUNI', 'AREAM5000': 'AREAM5000', 'CODICOMAR': 'CODICOMAR', 'NOMCOMAR': 'NOMCOMAR', 'CAPCOMAR': 'CAPCOMAR', 'CODIVEGUE': 'CODIVEGUE', 'NOMVEGUE': 'NOMVEGUE', 'CAPVEGUE': 'CAPVEGUE', 'CODIPROV': 'CODIPROV', 'NOMPROV': 'NOMPROV', 'CAPPROV': 'CAPPROV', });
-lyr_VescomtatdeCabrera_6.set('fieldAliases', {'CODIMUNI': 'CODIMUNI', 'NOMMUNI': 'NOMMUNI', 'CAPMUNI': 'CAPMUNI', 'AREAM5000': 'AREAM5000', 'CODICOMAR': 'CODICOMAR', 'NOMCOMAR': 'NOMCOMAR', 'CAPCOMAR': 'CAPCOMAR', 'CODIVEGUE': 'CODIVEGUE', 'NOMVEGUE': 'NOMVEGUE', 'CAPVEGUE': 'CAPVEGUE', 'CODIPROV': 'CODIPROV', 'NOMPROV': 'NOMPROV', 'CAPPROV': 'CAPPROV', });
-lyr_VescomtatdeRocabert_7.set('fieldAliases', {'CODIMUNI': 'CODIMUNI', 'NOMMUNI': 'NOMMUNI', 'CAPMUNI': 'CAPMUNI', 'AREAM5000': 'AREAM5000', 'CODICOMAR': 'CODICOMAR', 'NOMCOMAR': 'NOMCOMAR', 'CAPCOMAR': 'CAPCOMAR', 'CODIVEGUE': 'CODIVEGUE', 'NOMVEGUE': 'NOMVEGUE', 'CAPVEGUE': 'CAPVEGUE', 'CODIPROV': 'CODIPROV', 'NOMPROV': 'NOMPROV', 'CAPPROV': 'CAPPROV', });
-lyr_BaroniadEntena_8.set('fieldAliases', {'CODIMUNI': 'CODIMUNI', 'NOMMUNI': 'NOMMUNI', 'CAPMUNI': 'CAPMUNI', 'AREAM5000': 'AREAM5000', 'CODICOMAR': 'CODICOMAR', 'NOMCOMAR': 'NOMCOMAR', 'CAPCOMAR': 'CAPCOMAR', 'CODIVEGUE': 'CODIVEGUE', 'NOMVEGUE': 'NOMVEGUE', 'CAPVEGUE': 'CAPVEGUE', 'CODIPROV': 'CODIPROV', 'NOMPROV': 'NOMPROV', 'CAPPROV': 'CAPPROV', });
-lyr_ComtatdUrgell_9.set('fieldAliases', {'CODIMUNI': 'CODIMUNI', 'NOMMUNI': 'NOMMUNI', 'CAPMUNI': 'CAPMUNI', 'AREAM5000': 'AREAM5000', 'CODICOMAR': 'CODICOMAR', 'NOMCOMAR': 'NOMCOMAR', 'CAPCOMAR': 'CAPCOMAR', 'CODIVEGUE': 'CODIVEGUE', 'NOMVEGUE': 'NOMVEGUE', 'CAPVEGUE': 'CAPVEGUE', 'CODIPROV': 'CODIPROV', 'NOMPROV': 'NOMPROV', 'CAPPROV': 'CAPPROV', });
-lyr_VescomtatdeBas_3.set('fieldImages', {'CODIMUNI': '', 'NOMMUNI': '', 'CAPMUNI': '', 'AREAM5000': '', 'CODICOMAR': '', 'NOMCOMAR': '', 'CAPCOMAR': '', 'CODIVEGUE': '', 'NOMVEGUE': '', 'CAPVEGUE': '', 'CODIPROV': '', 'NOMPROV': '', 'CAPPROV': '', });
-lyr_SenyoriudUrtx_4.set('fieldImages', {'CODIMUNI': '', 'NOMMUNI': '', 'CAPMUNI': '', 'AREAM5000': '', 'CODICOMAR': '', 'NOMCOMAR': '', 'CAPCOMAR': '', 'CODIVEGUE': '', 'NOMVEGUE': '', 'CAPVEGUE': '', 'CODIPROV': '', 'NOMPROV': '', 'CAPPROV': '', });
-lyr_VescomtatdeCardona_5.set('fieldImages', {'CODIMUNI': '', 'NOMMUNI': '', 'CAPMUNI': '', 'AREAM5000': '', 'CODICOMAR': '', 'NOMCOMAR': '', 'CAPCOMAR': '', 'CODIVEGUE': '', 'NOMVEGUE': '', 'CAPVEGUE': '', 'CODIPROV': '', 'NOMPROV': '', 'CAPPROV': '', });
-lyr_VescomtatdeCabrera_6.set('fieldImages', {'CODIMUNI': '', 'NOMMUNI': '', 'CAPMUNI': '', 'AREAM5000': '', 'CODICOMAR': '', 'NOMCOMAR': '', 'CAPCOMAR': '', 'CODIVEGUE': '', 'NOMVEGUE': '', 'CAPVEGUE': '', 'CODIPROV': '', 'NOMPROV': '', 'CAPPROV': '', });
-lyr_VescomtatdeRocabert_7.set('fieldImages', {'CODIMUNI': '', 'NOMMUNI': '', 'CAPMUNI': '', 'AREAM5000': '', 'CODICOMAR': '', 'NOMCOMAR': '', 'CAPCOMAR': '', 'CODIVEGUE': '', 'NOMVEGUE': '', 'CAPVEGUE': '', 'CODIPROV': '', 'NOMPROV': '', 'CAPPROV': '', });
-lyr_BaroniadEntena_8.set('fieldImages', {'CODIMUNI': '', 'NOMMUNI': '', 'CAPMUNI': '', 'AREAM5000': '', 'CODICOMAR': '', 'NOMCOMAR': '', 'CAPCOMAR': '', 'CODIVEGUE': '', 'NOMVEGUE': '', 'CAPVEGUE': '', 'CODIPROV': '', 'NOMPROV': '', 'CAPPROV': '', });
-lyr_ComtatdUrgell_9.set('fieldImages', {'CODIMUNI': '', 'NOMMUNI': '', 'CAPMUNI': '', 'AREAM5000': '', 'CODICOMAR': '', 'NOMCOMAR': '', 'CAPCOMAR': '', 'CODIVEGUE': '', 'NOMVEGUE': '', 'CAPVEGUE': '', 'CODIPROV': '', 'NOMPROV': '', 'CAPPROV': '', });
-lyr_VescomtatdeBas_3.set('fieldLabels', {'CODIMUNI': 'no label', 'NOMMUNI': 'no label', 'CAPMUNI': 'no label', 'AREAM5000': 'no label', 'CODICOMAR': 'no label', 'NOMCOMAR': 'no label', 'CAPCOMAR': 'no label', 'CODIVEGUE': 'no label', 'NOMVEGUE': 'no label', 'CAPVEGUE': 'no label', 'CODIPROV': 'no label', 'NOMPROV': 'no label', 'CAPPROV': 'no label', });
-lyr_SenyoriudUrtx_4.set('fieldLabels', {'CODIMUNI': 'no label', 'NOMMUNI': 'no label', 'CAPMUNI': 'no label', 'AREAM5000': 'no label', 'CODICOMAR': 'no label', 'NOMCOMAR': 'no label', 'CAPCOMAR': 'no label', 'CODIVEGUE': 'no label', 'NOMVEGUE': 'no label', 'CAPVEGUE': 'no label', 'CODIPROV': 'no label', 'NOMPROV': 'no label', 'CAPPROV': 'no label', });
-lyr_VescomtatdeCardona_5.set('fieldLabels', {'CODIMUNI': 'no label', 'NOMMUNI': 'no label', 'CAPMUNI': 'no label', 'AREAM5000': 'no label', 'CODICOMAR': 'no label', 'NOMCOMAR': 'no label', 'CAPCOMAR': 'no label', 'CODIVEGUE': 'no label', 'NOMVEGUE': 'no label', 'CAPVEGUE': 'no label', 'CODIPROV': 'no label', 'NOMPROV': 'no label', 'CAPPROV': 'no label', });
-lyr_VescomtatdeCabrera_6.set('fieldLabels', {'CODIMUNI': 'no label', 'NOMMUNI': 'no label', 'CAPMUNI': 'no label', 'AREAM5000': 'no label', 'CODICOMAR': 'no label', 'NOMCOMAR': 'no label', 'CAPCOMAR': 'no label', 'CODIVEGUE': 'no label', 'NOMVEGUE': 'no label', 'CAPVEGUE': 'no label', 'CODIPROV': 'no label', 'NOMPROV': 'no label', 'CAPPROV': 'no label', });
-lyr_VescomtatdeRocabert_7.set('fieldLabels', {'CODIMUNI': 'no label', 'NOMMUNI': 'no label', 'CAPMUNI': 'no label', 'AREAM5000': 'no label', 'CODICOMAR': 'no label', 'NOMCOMAR': 'no label', 'CAPCOMAR': 'no label', 'CODIVEGUE': 'no label', 'NOMVEGUE': 'no label', 'CAPVEGUE': 'no label', 'CODIPROV': 'no label', 'NOMPROV': 'no label', 'CAPPROV': 'no label', });
-lyr_BaroniadEntena_8.set('fieldLabels', {'CODIMUNI': 'no label', 'NOMMUNI': 'no label', 'CAPMUNI': 'no label', 'AREAM5000': 'no label', 'CODICOMAR': 'no label', 'NOMCOMAR': 'no label', 'CAPCOMAR': 'no label', 'CODIVEGUE': 'no label', 'NOMVEGUE': 'no label', 'CAPVEGUE': 'no label', 'CODIPROV': 'no label', 'NOMPROV': 'no label', 'CAPPROV': 'no label', });
-lyr_ComtatdUrgell_9.set('fieldLabels', {'CODIMUNI': 'hidden field', 'NOMMUNI': 'inline label - visible with data', 'CAPMUNI': 'inline label - visible with data', 'AREAM5000': 'inline label - visible with data', 'CODICOMAR': 'hidden field', 'NOMCOMAR': 'hidden field', 'CAPCOMAR': 'hidden field', 'CODIVEGUE': 'hidden field', 'NOMVEGUE': 'inline label - visible with data', 'CAPVEGUE': 'hidden field', 'CODIPROV': 'hidden field', 'NOMPROV': 'inline label - visible with data', 'CAPPROV': 'hidden field', });
+var layersList = [group_Mapesbase,group_Corts1300];
+lyr_VescomtatdeBas_3.set('fieldAliases', {'CODI': 'CODI', 'Cort': 'Cort', 'Titular': 'Titular', 'Trobadors relacionats': 'Trobadors relacionats', 'Enllaç NARPAN': 'Enllaç NARPAN', });
+lyr_SenyoriudUrtx_4.set('fieldAliases', {'CODI': 'CODI', 'Cort': 'Cort', 'Titular': 'Titular', 'Trobadors relacionats': 'Trobadors relacionats', 'Enllaç NARPAN': 'Enllaç NARPAN', });
+lyr_VescomtatdeCardona_5.set('fieldAliases', {'CODI': 'CODI', 'Cort': 'Cort', 'Titular': 'Titular', 'Trobadors relacionats': 'Trobadors relacionats', 'Enllaç NARPAN': 'Enllaç NARPAN', });
+lyr_VescomtatdeCabrera_6.set('fieldAliases', {'CODI': 'CODI', 'Cort': 'Cort', 'Titular': 'Titular', 'Trobadors relacionats': 'Trobadors relacionats', 'Enllaç NARPAN': 'Enllaç NARPAN', });
+lyr_VescomtatdeRocabert_7.set('fieldAliases', {'CODI': 'CODI', 'Cort': 'Cort', 'Titular': 'Titular', 'Trobadors relacionats': 'Trobadors relacionats', 'Enllaç NARPAN': 'Enllaç NARPAN', });
+lyr_BaroniadEntena_8.set('fieldAliases', {'CODI': 'CODI', 'Cort': 'Cort', 'Titular': 'Titular', 'Trobadors relacionats': 'Trobadors relacionats', 'Enllaç NARPAN': 'Enllaç NARPAN', });
+lyr_ComtatdUrgell_9.set('fieldAliases', {'CODIMUNI': 'CODIMUNI', 'Cort': 'Cort', 'Titular': 'Titular', 'Trobadors relacionats': 'Trobadors relacionats', 'Enllaç NARPAN': 'Enllaç NARPAN', });
+lyr_VescomtatdeBas_3.set('fieldImages', {'CODI': '', 'Cort': '', 'Titular': '', 'Trobadors relacionats': '', 'Enllaç NARPAN': '', });
+lyr_SenyoriudUrtx_4.set('fieldImages', {'CODI': '', 'Cort': '', 'Titular': '', 'Trobadors relacionats': '', 'Enllaç NARPAN': '', });
+lyr_VescomtatdeCardona_5.set('fieldImages', {'CODI': '', 'Cort': '', 'Titular': '', 'Trobadors relacionats': '', 'Enllaç NARPAN': '', });
+lyr_VescomtatdeCabrera_6.set('fieldImages', {'CODI': '', 'Cort': '', 'Titular': '', 'Trobadors relacionats': '', 'Enllaç NARPAN': '', });
+lyr_VescomtatdeRocabert_7.set('fieldImages', {'CODI': '', 'Cort': '', 'Titular': '', 'Trobadors relacionats': '', 'Enllaç NARPAN': '', });
+lyr_BaroniadEntena_8.set('fieldImages', {'CODI': '', 'Cort': '', 'Titular': '', 'Trobadors relacionats': '', 'Enllaç NARPAN': '', });
+lyr_ComtatdUrgell_9.set('fieldImages', {'CODIMUNI': '', 'Cort': '', 'Titular': '', 'Trobadors relacionats': '', 'Enllaç NARPAN': '', });
+lyr_VescomtatdeBas_3.set('fieldLabels', {'CODI': 'hidden field', 'Cort': 'inline label - visible with data', 'Titular': 'inline label - visible with data', 'Trobadors relacionats': 'inline label - visible with data', 'Enllaç NARPAN': 'inline label - visible with data', });
+lyr_SenyoriudUrtx_4.set('fieldLabels', {'CODI': 'hidden field', 'Cort': 'inline label - visible with data', 'Titular': 'inline label - visible with data', 'Trobadors relacionats': 'inline label - visible with data', 'Enllaç NARPAN': 'inline label - visible with data', });
+lyr_VescomtatdeCardona_5.set('fieldLabels', {'CODI': 'hidden field', 'Cort': 'inline label - visible with data', 'Titular': 'inline label - visible with data', 'Trobadors relacionats': 'inline label - visible with data', 'Enllaç NARPAN': 'inline label - visible with data', });
+lyr_VescomtatdeCabrera_6.set('fieldLabels', {'CODI': 'hidden field', 'Cort': 'inline label - visible with data', 'Titular': 'inline label - visible with data', 'Trobadors relacionats': 'inline label - visible with data', 'Enllaç NARPAN': 'inline label - visible with data', });
+lyr_VescomtatdeRocabert_7.set('fieldLabels', {'CODI': 'hidden field', 'Cort': 'inline label - visible with data', 'Titular': 'inline label - visible with data', 'Trobadors relacionats': 'inline label - visible with data', 'Enllaç NARPAN': 'inline label - visible with data', });
+lyr_BaroniadEntena_8.set('fieldLabels', {'CODI': 'hidden field', 'Cort': 'inline label - visible with data', 'Titular': 'inline label - visible with data', 'Trobadors relacionats': 'inline label - visible with data', 'Enllaç NARPAN': 'inline label - visible with data', });
+lyr_ComtatdUrgell_9.set('fieldLabels', {'CODIMUNI': 'hidden field', 'Cort': 'inline label - visible with data', 'Titular': 'inline label - visible with data', 'Trobadors relacionats': 'inline label - visible with data', 'Enllaç NARPAN': 'inline label - visible with data', });
 lyr_ComtatdUrgell_9.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
